9821d37c8a7eb0b2c7ddcf8d7be668c4fbd6ca94
amakazor <amakazor@gmail.com>
2023-02-17 23:03:08 +0100

    Alias setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/"),
+            "@utility": path.resolve(__dirname, "./src/utility/"),
+            "@components": path.resolve(__dirname, "./src/components/"),
+            "@data": path.resolve(__dirname, "./src/data/"),
+        },
+    },
 });
