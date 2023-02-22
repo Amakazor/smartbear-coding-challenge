36e20c05f5e05aae4ecb91b62073b66d9ac6a2ab
amakazor <amakazor@gmail.com>
2023-02-23 00:20:52 +0100

    Paths page finish

--- src/routes/paths.tsx
+++ src/routes/paths.tsx
@@ -1,7 +1,20 @@
+import { ConditionalRenderer } from "@components/conditional-renderer";
+import { Paths as PathsComponent } from "@components/paths";
+import { TableOfContents } from "@components/table-of-contents";
+import { openApiContext } from "@context";
+import { toPairs } from "lodash";
+import { useContext } from "react";
+
 export const Paths = () => {
+    const { apiData: { Paths }, state } = useContext(openApiContext);
+    const pathsData = toPairs(Paths);
+
     return (
-        <div className={""}>
-            Paths
-        </div>
+        <ConditionalRenderer currentState={state}>
+            <div className={"flex flex-col w-full items-center gap-12 max-w-screen-xl px-8 pt-8"}>
+                <TableOfContents className={"max-w-screen-xl w-full"} items={pathsData.map(([id]) => ({ id }))}/>
+                <PathsComponent title={"All paths"} paths={pathsData}/>
+            </div>
+        </ConditionalRenderer>
     );
 };
